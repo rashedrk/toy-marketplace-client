5d373208a9aac2243edcf7e6c5a552c525d3fbe7
rashedrk <rashedrk001@gmail.com>
2023-05-18 22:39:56 +0600

    auth provider

---
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import { Children, createContext, useState } from "react";
+import {getAuth} from "firebase/auth";
+import app from '../../firebase/firebase.config'
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const auth = getAuth(app);
+    const [user,setUser] = useState(null);
+
+
+
+
+    const authInfo = {
+        user,
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
