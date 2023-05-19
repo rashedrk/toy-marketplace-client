e61079f165ded9676bad2a50409967a1685dc3f4
rashedrk <rashedrk001@gmail.com>
2023-05-19 12:57:58 +0600

    register page update

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,17 +1,31 @@
 
 import { Link } from 'react-router-dom';
 import './Register.css'
+import { useState } from 'react';
 const Register = () => {
+    const [error, setError] = useState('')
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
 
+        if (password !== confirmPassword) {
+            return setError('Password and Confirm Password do not match')
+        }
+        console.log(name,email,photo, password,confirmPassword);
+    }
     return (
-        <div>
-            <div className="hero min-h-screen bg-[#C2E0EB]">
+            <div className="hero py-10 min-h-screen bg-[#C2E0EB]">
                 <div className="hero-content flex-col ">
                     <div className="text-center mb-5">
-                        <h1 className="text-5xl font-bold">Register Here!</h1>
+                        <h1 className="text-5xl font-extrabold">Register Here!</h1>
                     </div>
                     <div className="card  w-full max-w-full shadow-2xl bg-base-100">
-                        <form className="card-body w-full">
+                        <form onSubmit={handleRegister} className="card-body w-full">
                             <div className='grid gap-3 grid-cols-2'>
                                 <div className="form-control">
                                 <label className="label">
@@ -23,7 +37,7 @@ const Register = () => {
                                 <label className="label">
                                     <span className="label-text">Photo Url</span>
                                 </label>
-                                <input type="text" name='photoUrl' placeholder="Enter Your Picture" className="input input-bordered" />
+                                <input type="text" name='photo' placeholder="Enter Your Picture" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -45,7 +59,7 @@ const Register = () => {
                             </div>
                             </div>
                             <div className="form-control ms-2 my-2">
-                                <p className='text-sm text-red-600'></p>
+                                <p className='text-sm text-red-600'>{error}</p>
                             </div>
                             <div className="form-control mt-2">
                                 <button className="btn primary-btn">Register</button>
@@ -58,7 +72,6 @@ const Register = () => {
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
