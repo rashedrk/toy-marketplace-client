0b268fa1c651af8b9f890b4ce28fae3f734fab2c
rashedrk <rashedrk001@gmail.com>
2023-05-19 11:36:24 +0600

    simple footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,29 @@
 
 const Footer = () => {
     return (
-        <div>
-            footer here
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img className="w-72" src="/src/assets/logo.png" alt="" />
+                <p>KidsLand Ltd.<br />Providing kids toy since 1999</p>
+            </div>
+            <div>
+                <span className="footer-title">Social Links</span>
+                <a className="link link-hover">Facebook</a>
+                <a className="link link-hover">Twitter</a>
+                <a className="link link-hover">Instagram</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
